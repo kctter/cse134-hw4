2d1fa63065fd9dedb3c52cbea540f626700a9694
kctter <chanchanheng@outlook.com>
2022-07-27 18:49:16 -0700

    added blog

---
+++ scripts/examples.js
@@ -0,0 +1,29 @@
+/**
+ * `export default` means that we export this object (an array in this case) as the module's contents. Note that it has no name!
+ */
+export default [
+    {
+        "title": "Modern Software Engineering",
+        "date": "2020-01-01",
+        "summary": "Writing for programmers, managers, and technical leads at all levels of experience, Farley illuminates durable principles at the heart of effective software development. He distills the discipline into two core exercises: learning and exploration and managing complexity. For each, he defines principles that can help you improve everything from your mindset to the quality of your code, and describes approaches proven to promote success."
+
+    }
+    // {
+    //     "title": "The Mythical Man Month",
+    //     "author": "Fred Brooks",
+    //     "cover": "img/mythical-man-month.jpg",
+    //     "comments": "The Mythical Man-Month: Essays on Software Engineering is a book on software engineering and project management by Fred Brooks first published in 1975, with subsequent editions in 1982 and 1995. Its central theme is that \"adding manpower to a late software project makes it later.\" This idea is known as Brooks's law, and is presented along with the second-system effect and advocacy of prototyping."
+    // },
+    // {
+    //     "title": "The Pragmatic Programmer",
+    //     "author": "Andrew Hunt & David Thomas",
+    //     "cover": "img/pragmatic-programmer.jpg",
+    //     "comments": "The Pragmatic Programmer: From Journeyman to Master is a book about computer programming and software engineering, written by Andrew Hunt and David Thomas and published in October 1999. The book does not present a systematic theory, but rather a collection of tips to improve the development process in a pragmatic way. The main qualities of what the authors refer to as a pragmatic programmer are being an early adopter, to have fast adaptation, inquisitiveness and critical thinking, realism, and being a jack-of-all-trades."
+    // },
+    // {
+    //     "title": "The Reflective Practitioner",
+    //     "author": "Donald A. Schon",
+    //     "cover": "img/reflective-practitioner.jpg",
+    //     "comments": "A leading M.I.T. social scientist and consultant examines five professions (engineering, architecture, management, psychotherapy, and town planning) to show how professionals really go about solving problems. The best professionals, Donald Schon maintains, know more than they can put into words. To meet the challenges of their work, they rely less on formulas learned in graduate school than on the kind of improvisation learned in practice."
+    // }
+];
